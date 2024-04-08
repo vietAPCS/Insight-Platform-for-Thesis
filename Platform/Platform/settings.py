89175917cf14b9d169836a138272d32027d44d8b
vhdat <81861683+vhdat@users.noreply.github.com>
2024-04-08 21:50:09 +0700

    copy + paste

--- Platform/Platform/settings.py
+++ Platform/Platform/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Community',
+    'Member',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -74,10 +77,15 @@ WSGI_APPLICATION = 'Platform.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': {        
+        'ENGINE': 'django.db.backends.mysql',        
+        'NAME': 'insight_database',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"},
+   }
 }
 
 
--- Platform/Platform/urls.py
+++ Platform/Platform/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include(('Community.urls', 'Commnuity'), namespace='Community')),
+    path('', include(('Member.urls', 'Member'), namespace='Member')),
     path('admin/', admin.site.urls),
-    path('', include('Community.urls'))
 ]
