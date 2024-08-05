b54b36f8ace452816c7b1e25525af093e50f85d0
Viet <87920454+vietAPCS@users.noreply.github.com>
2024-08-05 21:52:01 +0700

    some changes

--- Platform/Community/static/transaction.js
+++ Platform/Community/static/transaction.js
@@ -1,8 +1,18 @@
 
+network = process.env.ETHEREUM_NETWORK;
 const web3 = new Web3(window.ethereum);
 
-const contractAddress = '0x58Be5eD51e171D50C958b78ff3E9A093c287D2EB';
+const web32 = new Web3(
+    new Web3.providers.HttpProvider(
+      `https://${network}.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    ),
+  );
+// Creating a signing account from a private key
+const signer = web32.eth.accounts.privateKeyToAccount(process.env.SIGNER_PRIVATE_KEY);
+web32.eth.accounts.wallet.add(signer);
 
+const contractAddress = '0x58Be5eD51e171D50C958b78ff3E9A093c287D2EB';
+const vaultAddress = '0x64E21641D63Fc270FA44cC8f3e61Fe177b6a6864';
 const contractABI = [
 	{
 		"inputs": [
@@ -416,8 +426,127 @@ const contractABI = [
 		"type": "function"
 	}
 ];
+const vaultABI= [
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			}
+		],
+		"name": "OwnableInvalidOwner",
+		"type": "error"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "account",
+				"type": "address"
+			}
+		],
+		"name": "OwnableUnauthorizedAccount",
+		"type": "error"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "previousOwner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "OwnershipTransferred",
+		"type": "event"
+	},
+	{
+		"inputs": [],
+		"name": "gameToken",
+		"outputs": [
+			{
+				"internalType": "contract IERC20",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "renounceOwnership",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "recepient",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "prize",
+				"type": "uint256"
+			}
+		],
+		"name": "rewardMember",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "transferOwnership",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
+];
 
 const Insights = new web3.eth.Contract(contractABI, contractAddress);
+const Vault = new web32.eth.Contract(vaultABI, vaultAddress);
 
 async function requestAccount() {
 	try {
@@ -468,4 +597,41 @@ async function buyDoc(address, docPrice, cid, title) {
     } catch (error) {
         console.error(error);
     }
+}
+
+async function rewardToken(address, value) {
+	console.log(address);
+	
+	// try {
+	// 	const fromAdress = 0x66372ecb44B1d778309dF2753c986fBCb0470745;
+		
+	// 	const result = await Vault.methods.rewardMember(address,correctValue).send({from: fromAdress});
+	// 	console.log(result);
+	// } catch (error) {
+	// 	console.error(error);
+	// }
+	const correctValue = value * 1000000000000000000;
+	const method_abi = Vault.methods.rewardMember(address,correctValue).encodeABI();
+	const tx = {
+	  from: signer.address,
+	  to: vaultAddress,
+	  data: method_abi,
+	  value: '0',
+	  gasPrice: '100000000000',
+	};
+	const gas_estimate = await web32.eth.estimateGas(tx);
+	tx.gas = gas_estimate;
+	const signedTx = await web3.eth.accounts.signTransaction(tx, signer.privateKey);
+	console.log("Raw transaction data: " + ( signedTx).rawTransaction);
+	// Sending the transaction to the network
+	const receipt = await web32.eth
+	  .sendSignedTransaction(signedTx.rawTransaction)
+	  .once("transactionHash", (txhash) => {
+		console.log(`Mining transaction ...`);
+		console.log(`https://${network}.etherscan.io/tx/${txhash}`);
+	  });
+	
+
+
+
 }
